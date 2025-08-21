66c02983cfd2c7fd67f528f6714418b62e63e153
Graeme Blair <graemeblair@users.noreply.github.com>
2025-08-20 18:00:07 -0700

    add detentions file

---
+++ clean/detentions-latest.R
@@ -0,0 +1,85 @@
+# ---- Packages ----
+library(tidyverse)
+library(tidylog)
+
+# ---- Functions ----
+source("clean/functions/check_dttm_and_convert_to_date.R")
+source("clean/functions/is_not_blank_or_redacted.R")
+
+# ---- Read in to temporary file ----
+url <- "https://ucla.box.com/shared/static/c6tfgtq90er5df898vaq912okchn9o66.xlsx"
+f <- tempfile(fileext = ".xlsx")
+download.file(url, f, mode = "wb")
+
+col_types <- c(
+  "date", # Stay Book In Date Time
+  "date", # Book In Date Time
+  "text", # Detention Facility
+  "text", # Detention Facility Code
+  "date", # Detention Book Out Date Time
+  "date", # Stay Book Out Date Time
+  "text", # Detention Release Reason
+  "date", # Stay Book Out Date
+  "text", # Stay Release Reason
+  "text", # Religion
+  "text", # Gender
+  "text", # Marital Status
+  "text", # Birth Date (redacted)
+  "numeric", # Birth Year
+  "text", # Ethnicity
+  "text", # Entry Status
+  "text", # Felon
+  "date", # Bond Posted Date
+  "numeric", # Bond Posted Amount
+  "text", # Case Status
+  "text", # Case Category
+  "text", # Final Order Yes No
+  "date", # Final Order Date
+  "text", # Case Threat Level
+  "text", # Book In Criminality
+  "text", # Final Charge
+  "date", # Departed Date
+  "text", # Departure Country
+  "numeric", # Initial Bond Set Amount
+  "text", # Citizenship Country
+  "text", # Final Program
+  "text", # Most Serious Conviction (MSC) Charge Code
+  "text", # MSC Charge
+  "text", # Alien File Number (redacted)
+  "text", # EID Case ID (redacted)
+  "text", # EID Subject ID (redacted)
+  "text"  # Unique Identifier
+)
+
+detentions_df <- 
+  readxl::excel_sheets(path = f) |> 
+  set_names() |> 
+  map_dfr(~readxl::read_excel(path = f, sheet = 1, col_types = col_types, skip = 6), .id = "sheet")
+
+detentions_df2 <- 
+  detentions_df |> 
+  # clean names
+  janitor::clean_names(allow_dupes = FALSE) |>
+  # add file name
+  mutate(file = "2025-ICLI-00019_2024-ICFO-39357_ICE Detentions_LESA-STU_FINAL Redacted.xlsx") |>
+  # add row number from original file
+  mutate(row = as.integer(row_number() + 6 + 1)) |> 
+  # add identifier for each ICE stay, encompassing multiple detentions or stints
+  mutate(stay_ID = str_c(unique_identifier, "_", stay_book_in_date_time)) |>
+  # remove columns that are fully blank (all NA) or fully redacted
+  select(where(is_not_blank_or_redacted)) |> 
+  # convert dttm to date if there is no time information in the column
+  mutate(
+    across(where(~inherits(., "POSIXt")), check_dttm_and_convert_to_date)
+  ) |> 
+  mutate(
+    birth_year = as.integer(birth_year)
+  ) |> 
+  relocate(file, sheet, row, .after = last_col())
+
+# ---- Save Outputs ----
+
+arrow::write_feather(detentions_df, "outputs/detentions-latest.feather")
+writexl::write_xlsx(detentions_df, "outputs/detentions-latest.xlsx")
+haven::write_dta(detentions_df, "outputs/detentions-latest.dta")
+haven::write_sav(detentions_df, "outputs/detentions-latest.sav")
