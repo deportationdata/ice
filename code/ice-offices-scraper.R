f0af607efba58126912bb139b4b57f8652cfb8dc
Graeme Blair <graemeblair@users.noreply.github.com>
2025-09-21 12:26:59 -0700

    simplify scraper

--- code/ice-offices-scraper.R
+++ code/ice-offices-scraper.R
@@ -10,10 +10,10 @@ library(tidygeocoder)
 scrape_field_offices <- function(url) {
   results <- GET(url)
   page <- read_html(content(results, as = "text"))
-  
+
   # get content for each field office
   field_offices <- page |> html_elements(".grid__content")
-  
+
   parse_field_office <- function(field_office) {
     area <- field_office |>
       html_element(".views-field.views-field-body .field-content") |>
@@ -34,11 +34,11 @@ scrape_field_offices <- function(url) {
     zip <- field_office |>
       html_element(".postal-code") |>
       html_text(trim = TRUE)
-    
+
     # clean area
     area <- if (
       !is.na(area) &&
-      str_detect(area, regex("Area of Responsibility:", ignore_case = TRUE))
+        str_detect(area, regex("Area of Responsibility:", ignore_case = TRUE))
     ) {
       str_match(
         area,
@@ -52,7 +52,7 @@ scrape_field_offices <- function(url) {
     } else {
       NA
     }
-    
+
     tibble(
       area = area,
       office_name = str_remove(office_type, " - .*"),
@@ -72,20 +72,20 @@ scrape_field_offices <- function(url) {
       zip = str_extract(zip, "^\\d{5}")
     )
   }
-  
+
   # bind into one tibble
   field_office_df <- map_dfr(field_offices, parse_field_office)
-  
+
   return(field_office_df)
 }
 
 scrape_sub_offices <- function(url) {
   results <- GET(url)
   page <- read_html(content(results, as = "text"))
-  
+
   # get content for each sub-office
   sub_offices <- page |> html_elements(".grid__content")
-  
+
   parse_sub_office <- function(sub_office) {
     area <- sub_office |>
       html_element(".views-field.views-field-body .field-content") |>
@@ -104,11 +104,11 @@ scrape_sub_offices <- function(url) {
       html_element(".administrative-area") |>
       html_text(trim = TRUE)
     zip <- sub_office |> html_element(".postal-code") |> html_text(trim = TRUE)
-    
+
     # clean area
     area <- if (
       !is.na(area) &&
-      str_detect(area, regex("Area Coverage:", ignore_case = TRUE))
+        str_detect(area, regex("Area Coverage:", ignore_case = TRUE))
     ) {
       str_match(
         area,
@@ -122,7 +122,7 @@ scrape_sub_offices <- function(url) {
     } else {
       NA
     }
-    
+
     tibble(
       area = area,
       main_office = main_office,
@@ -141,10 +141,10 @@ scrape_sub_offices <- function(url) {
       zip = str_extract(zip, "^\\d{5}")
     )
   }
-  
+
   # bind into one tibble
   sub_office_df <- map_dfr(sub_offices, parse_sub_office)
-  
+
   return(sub_office_df)
 }
 
@@ -152,15 +152,15 @@ scrape_sub_offices <- function(url) {
 get_num_pages <- function(url) {
   results <- GET(url)
   page <- read_html(content(results, as = "text"))
-  
+
   pages <- page |>
     html_element(".usa-pagination") |>
     html_text(trim = TRUE)
-  
+
   page_nums <- str_extract_all(pages, "\\d+") |>
     unlist() |>
     as.integer()
-  
+
   if (length(page_nums) == 0) {
     return(1)
   } else {
@@ -204,82 +204,9 @@ all_offices <-
   ) |>
   mutate(address_full = str_c(address, ", ", city, ", ", state, " ", zip_4))
 
-geocoded_census <-
-  all_offices |>
-  geocode(
-    address = address_full,
-    method = "census",
-    lat = latitude_census,
-    long = longitude_census
-  )
-
-not_geocoded_census <-
-  all_offices |>
-  anti_join(
-    geocoded_census |> filter(!is.na(latitude_census)),
-    by = c("office_category", "office_name", "office_type", "main_office", "address", "city", "state", "zip", "zip_4", "area", "address_full")
-  )
-
-Sys.setenv(GOOGLEGEOCODE_API_KEY = "AIzaSyBEfZio8zgA7S7_irwkcGkNMFKyo0_gYBM")
-
-geocoded_google <-
-  not_geocoded_census |>
-  geocode(
-    address = address_full,
-    method = "google",
-    lat = latitude_google,
-    long = longitude_google
-  )
-
-not_geocoded_google <-
-  not_geocoded_census |>
-  anti_join(
-    geocoded_google |> filter(!is.na(latitude_google)),
-    by = c("office_category", "office_name", "office_type", "main_office", "address", "city", "state", "zip", "zip_4", "area", "address_full")
-  )
-
-geocoded_osm <-
-  not_geocoded_google |>
-  geocode(
-    address = address_full,
-    method = "osm",
-    lat = latitude_osm,
-    long = longitude_osm
-  )
-
-all_geocoded <-
-  bind_rows(
-    "census" = geocoded_census |>
-      select(
-        office_category:area,
-        address_full,
-        latitude = latitude_census,
-        longitude = longitude_census
-      ),
-    "google" = geocoded_google |>
-      select(
-        office_category:area,
-        address_full,
-        latitude = latitude_google,
-        longitude = longitude_google
-      ),
-    "osm" = geocoded_osm |>
-      select(
-        office_category:area,
-        address_full,
-        latitude = latitude_osm,
-        longitude = longitude_osm
-      ),
-    .id = "geocode_source"
-  ) |>
-  filter(!is.na(latitude))
-
-# ungeocoded addresses
-ungeocoded_addresses <-
-  not_geocoded_google |>
-  anti_join(
-    geocoded_osm |> filter(!is.na(latitude_osm)),
-    by = c("office_category", "office_type", "main_office", "address", "city", "state", "zip", "zip_4", "area", "address_full")
-  )
-
-arrow::write_feather(all_geocoded, "data/ice-offices.feather")
+# only save if there are new offices
+existing_offices <- arrow::read_feather("data/ice-offices.feather")
+new_offices <- anti_join(all_offices, existing_offices)
+if (nrow(new_offices) > 0 || nrow(existing_offices) != nrow(all_offices)) {
+  arrow::write_feather(all_offices, "data/ice-offices.feather")
+}
