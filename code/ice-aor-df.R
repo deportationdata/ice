3da8388f8f82e68f3b29b4ba2d7754be215af581
hayleylabia <162515839+hayleylabia@users.noreply.github.com>
2025-09-23 17:09:36 -0700

    Create ice-offices-scraper (#3)
    * Create ice-offices-scraper
    
    * scraper workflow
    
    * ed
    
    * ed
    
    * Update ice-offices-scraper.R
    
    * add save file
    
    * ed
    
    * ed
    
    * ed
    
    * ws
    
    * update packages
    
    * ed
    
    * add ice offices data
    
    * Update ice-offices-scraper.R
    
    * ed
    
    * ws and reorg the bind
    
    * add county-aor-df made manually
    
    * add ice aor df maker
    
    * update offices scraper and data
    
    * ed
    
    * add reverse geocode
    
    * Update ice-offices-scraper.R
    
    * simplify scraper
    
    * ice offices cleanup
    
    * geocoded
    
    * move geocoder into scraper
    
    * Delete code/geography.R
    
    * alternative file types
    
    * remove indiv count
    
    * ice offices saved
    
    * aor
    
    * switch to separate PRs
    
    ---------
    
    Co-authored-by: itsh4yl3y <162515839+itsh4yl3y@users.noreply.github.com>
    Co-authored-by: Graeme Blair <graemeblair@users.noreply.github.com>

---
+++ code/ice-aor-df.R
@@ -0,0 +1,185 @@
+library(tidyverse)
+library(sf)
+library(tigris)
+library(sfarrow)
+
+aor_labels_df <-
+  tibble::tribble(
+    ~`Area of Responsibility`,
+    ~`AOR Abbreviation`,
+    "Atlanta",
+    "ATL",
+    "Baltimore",
+    "BAL",
+    "Boston",
+    "BOS",
+    "Buffalo",
+    "BUF",
+    "Chicago",
+    "CHI",
+    "Dallas",
+    "DAL",
+    "Denver",
+    "DEN",
+    "Detroit",
+    "DET",
+    "El Paso",
+    "ELP",
+    "Harlingen",
+    "HAL",
+    "Houston",
+    "HOU",
+    "HQ",
+    "HQ",
+    "Los Angeles",
+    "LOS",
+    "Miami",
+    "MIA",
+    "Newark",
+    "NEW",
+    "New Orleans",
+    "NOL",
+    "New York City",
+    "NYC",
+    "Philadelphia",
+    "PHI",
+    "Phoenix",
+    "PHO",
+    "Seattle",
+    "SEA",
+    "San Francisco",
+    "SFR",
+    "Salt Lake City",
+    "SLC",
+    "San Antonio",
+    "SNA",
+    "San Diego",
+    "SND",
+    "St. Paul",
+    "SPM",
+    "Washington",
+    "WAS"
+  ) |>
+  mutate(
+    `Area of Responsibility` = str_c(
+      `Area of Responsibility`,
+      " Area of Responsibility"
+    )
+  )
+
+county_aor_df <- read_csv(
+  "https://raw.githubusercontent.com/UWCHR/ice-enforce/main/share/hand/county_aor.csv",
+  progress = FALSE
+)
+
+# Change counties to HAR if in Harlingen AOR
+aor_df <-
+  county_aor_df |>
+  mutate(
+    aor = case_when(
+      name %in%
+        c(
+          "cameron county, texas",
+          "willacy county, texas",
+          "kenedy county, texas",
+          "kleberg county, texas",
+          "nueces county, texas",
+          "san patricio county, texas",
+          "hidalgo county, texas",
+          "brooks county, texas",
+          "starr county, texas",
+          "jim hogg county, texas",
+          "zapata county, texas",
+          "webb county, texas",
+          "duval county, texas",
+          "jim wells county, texas",
+          "aransas county, texas"
+        ) ~
+        "HAL",
+      TRUE ~ aor
+    )
+  ) |>
+  left_join(
+    aor_labels_df |> select(`Area of Responsibility`, `AOR Abbreviation`),
+    by = c("aor" = "AOR Abbreviation")
+  ) |>
+  select(-aor)
+
+counties_sf <- counties(cb = TRUE, year = 2024, class = "sf", progress = FALSE)
+
+# note valdez-cordova census area is in aor_df but not the counties file because it no longer exists
+# the aor file does have the two it was split into, copper river and chugach
+aor_sf <-
+  counties_sf |>
+  left_join(aor_df, by = c("GEOID" = "geoid")) |>
+  mutate(
+    `Area of Responsibility` = case_when(
+      STUSPS %in% c("AK", "HI", "MP", "GU") ~
+        "San Francisco Area of Responsibility",
+      STUSPS == "VI" ~ "Miami Area of Responsibility",
+      STUSPS == "AS" ~ NA_character_,
+      TRUE ~ `Area of Responsibility`
+    )
+  ) |>
+  filter(!is.na(`Area of Responsibility`)) |>
+  group_by(`Area of Responsibility`) |>
+  summarize(geometry = st_union(geometry), .groups = "drop") |>
+  st_cast("MULTIPOLYGON")
+
+hq_aor_df <- tibble(`Area of Responsibility` = "HQ Area of Responsibility")
+
+aor_sf <-
+  bind_rows(aor_sf, hq_aor_df)
+
+sfarrow::st_write_feather(aor_sf, "data/ice-aor-shp.feather")
+
+temp_dir <- tempdir()
+temp_shp_path <- file.path(temp_dir, "ice-aor-shp.shp")
+st_write(aor_sf, temp_shp_path, append = FALSE)
+
+# create a zip file with all necessary shapefile components
+zip(
+  zipfile = "data/ice-aor-shp.zip",
+  files = c(
+    file.path(temp_dir, "ice-aor-shp.shp"),
+    file.path(temp_dir, "ice-aor-shp.dbf"),
+    file.path(temp_dir, "ice-aor-shp.prj"),
+    file.path(temp_dir, "ice-aor-shp.shx")
+  )
+)
+
+# verification
+
+library(validate)
+
+arrests <- arrow::read_feather("data/arrests-latest.feather")
+
+rules <-
+  validator(
+    n_aors_agrees = sum(in_arrests) == sum(in_aor_sf), # There should be 93 districts - If not, bind may have failed
+    n_aors_26 = sum(in_arrests) == 26
+  )
+
+arrests |>
+  filter(!is.na(apprehension_aor)) |>
+  distinct(apprehension_aor) |>
+  mutate(in_arrests = 1) |>
+  left_join(
+    aor_sf |>
+      as_tibble() |>
+      distinct(`Area of Responsibility`) |>
+      mutate(in_aor_sf = 1),
+    by = c("apprehension_aor" = "Area of Responsibility")
+  ) |>
+  confront(rules) |>
+  summary() |>
+  (\(x) {
+    if (any(x$fails > 0 | x$errors > 0)) {
+      stop(
+        "Validation failed: ",
+        paste(x$name[x$fails > 0 | x$errors > 0], collapse = ", ")
+      )
+    } else {
+      x
+    }
+  })()
