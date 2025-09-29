1f01e8fd30a9c18a8c8348c9fbfdf1475643a7c8
Graeme Blair <graemeblair@users.noreply.github.com>
2025-09-29 12:40:45 -0700

    update area of responsibility code to do it manually

--- code/ice-aor-df.R
+++ code/ice-aor-df.R
@@ -3,138 +3,78 @@ library(sf)
 library(tigris)
 library(sfarrow)
 
-aor_labels_df <-
-  tibble::tribble(
-    ~`Area of Responsibility`,
-    ~`AOR Abbreviation`,
-    "Atlanta",
-    "ATL",
-    "Baltimore",
-    "BAL",
-    "Boston",
-    "BOS",
-    "Buffalo",
-    "BUF",
-    "Chicago",
-    "CHI",
-    "Dallas",
-    "DAL",
-    "Denver",
-    "DEN",
-    "Detroit",
-    "DET",
-    "El Paso",
-    "ELP",
-    "Harlingen",
-    "HAL",
-    "Houston",
-    "HOU",
-    "HQ",
-    "HQ",
-    "Los Angeles",
-    "LOS",
-    "Miami",
-    "MIA",
-    "Newark",
-    "NEW",
-    "New Orleans",
-    "NOL",
-    "New York City",
-    "NYC",
-    "Philadelphia",
-    "PHI",
-    "Phoenix",
-    "PHO",
-    "Seattle",
-    "SEA",
-    "San Francisco",
-    "SFR",
-    "Salt Lake City",
-    "SLC",
-    "San Antonio",
-    "SNA",
-    "San Diego",
-    "SND",
-    "St. Paul",
-    "SPM",
-    "Washington",
-    "WAS"
-  ) |>
-  mutate(
-    `Area of Responsibility` = str_c(
-      `Area of Responsibility`,
-      " Area of Responsibility"
-    )
-  )
-
-county_aor_df <- read_csv(
-  "https://raw.githubusercontent.com/UWCHR/ice-enforce/main/share/hand/county_aor.csv",
+counties_sf <- counties(
+  cb = TRUE,
+  year = 2024,
+  class = "sf",
   progress = FALSE
 )
 
-# Change counties to HAR if in Harlingen AOR
-aor_df <-
-  county_aor_df |>
-  mutate(
-    aor = case_when(
-      name %in%
-        c(
-          "cameron county, texas",
-          "willacy county, texas",
-          "kenedy county, texas",
-          "kleberg county, texas",
-          "nueces county, texas",
-          "san patricio county, texas",
-          "hidalgo county, texas",
-          "brooks county, texas",
-          "starr county, texas",
-          "jim hogg county, texas",
-          "zapata county, texas",
-          "webb county, texas",
-          "duval county, texas",
-          "jim wells county, texas",
-          "aransas county, texas"
-        ) ~
-        "HAL",
-      TRUE ~ aor
-    )
-  ) |>
-  left_join(
-    aor_labels_df |> select(`Area of Responsibility`, `AOR Abbreviation`),
-    by = c("aor" = "AOR Abbreviation")
-  ) |>
-  select(-aor)
+aor_df <- read_csv("inputs/ice-areas-of-responsibility.csv")
 
-counties_sf <- counties(cb = TRUE, year = 2024, class = "sf", progress = FALSE)
+aor_entire_states <-
+  aor_df |>
+  filter(is.na(county_name)) |>
+  select(-county_name)
 
-# note valdez-cordova census area is in aor_df but not the counties file because it no longer exists
-# the aor file does have the two it was split into, copper river and chugach
-aor_sf <-
+aor_partial_states <- aor_df |> filter(!is.na(county_name))
+
+aor_county_sf_partial_states <-
   counties_sf |>
-  left_join(aor_df, by = c("GEOID" = "geoid")) |>
-  mutate(
-    `Area of Responsibility` = case_when(
-      STUSPS %in% c("MP", "GU") ~ "San Francisco Area of Responsibility",
-      STUSPS == "VI" ~ "Miami Area of Responsibility",
-      STUSPS == "AS" ~ NA_character_,
-      TRUE ~ `Area of Responsibility`
-    )
+  inner_join(
+    aor_partial_states,
+    by = c("NAME" = "county_name", "STATE_NAME" = "state_name")
+  )
+# as_tibble() |>
+# filter(is.na(in_theirs) & !is.na(in_ours))
+
+aor_county_sf_entire_states <-
+  counties_sf |>
+  inner_join(
+    aor_entire_states,
+    by = c("STATE_NAME" = "state_name")
+  )
+
+aor_county_sf <-
+  bind_rows(
+    aor_county_sf_partial_states,
+    aor_county_sf_entire_states
   ) |>
-  filter(!is.na(`Area of Responsibility`)) |>
-  group_by(`Area of Responsibility`) |>
+  mutate(
+    area_of_responsibility_name = office_name
+  )
+
+# validate that all counties are assigned to an AOR except in American Samoa
+# counties_sf |>
+#   as_tibble() |>
+#   left_join(
+#     aor_sf |> mutate(in_aor_df = 1) |> as_tibble(),
+#     by = c("NAME", "STATE_NAME")
+#   ) |>
+#   filter(is.na(office_name)) |>
+#   select(1:10)
+
+hq_aor_df <- tibble(area_of_responsibility_name = "HQ")
+
+aor_county_sf <-
+  bind_rows(aor_county_sf, hq_aor_df)
+
+aor_sf <-
+  aor_county_sf |>
+  filter(!is.na(area_of_responsibility_name)) |>
+  group_by(office_name, area_of_responsibility_name) |>
   summarize(geometry = st_union(geometry), .groups = "drop") |>
   st_cast("MULTIPOLYGON")
-
-hq_aor_df <- tibble(`Area of Responsibility` = "HQ Area of Responsibility")
-
-aor_sf <-
-  bind_rows(aor_sf, hq_aor_df)
 
 sfarrow::st_write_feather(aor_sf, "data/ice-aor-shp.feather")
 
 temp_dir <- tempdir()
 temp_shp_path <- file.path(temp_dir, "ice-aor-shp.shp")
-st_write(aor_sf, temp_shp_path, append = FALSE)
+st_write(
+  aor_sf |> rename(aor_name = area_of_responsibility_name),
+  temp_shp_path,
+  append = FALSE
+)
 
 # create a zip file with all necessary shapefile components
 zip(
@@ -166,9 +106,9 @@ arrests |>
   left_join(
     aor_sf |>
       as_tibble() |>
-      distinct(`Area of Responsibility`) |>
+      distinct(area_of_responsibility_name) |>
       mutate(in_aor_sf = 1),
-    by = c("apprehension_aor" = "Area of Responsibility")
+    by = c("apprehension_aor" = "area_of_responsibility_name")
   ) |>
   confront(rules) |>
   summary() |>
