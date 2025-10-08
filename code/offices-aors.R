229df3ef5c455cf025a0f52038bdb0ac7f876915
Graeme Blair <graemeblair@users.noreply.github.com>
2025-10-07 19:31:39 -0700

    add aor county files

--- code/offices-aors.R
+++ code/offices-aors.R
@@ -25,8 +25,6 @@ aor_county_sf_partial_states <-
     aor_partial_states,
     by = c("NAME" = "county_name", "STATE_NAME" = "state_name")
   )
-# as_tibble() |>
-# filter(is.na(in_theirs) & !is.na(in_ours))
 
 aor_county_sf_entire_states <-
   counties_sf |>
@@ -58,6 +56,29 @@ hq_aor_df <- tibble(area_of_responsibility_name = "HQ")
 
 aor_county_sf <-
   bind_rows(aor_county_sf, hq_aor_df)
+
+sfarrow::st_write_feather(aor_county_sf, "data/ice-aor-county-shp.feather")
+
+temp_dir <- tempdir()
+temp_shp_path <- file.path(temp_dir, "ice-aor-county-shp.shp")
+st_write(
+  aor_sf |> rename(aor_name = area_of_responsibility_name),
+  temp_shp_path,
+  append = FALSE
+)
+
+# create a zip file with all necessary shapefile components
+zip(
+  zipfile = "data/ice-aor-county-shp.zip",
+  files = c(
+    file.path(temp_dir, "ice-aor-county-shp.shp"),
+    file.path(temp_dir, "ice-aor-county-shp.dbf"),
+    file.path(temp_dir, "ice-aor-county-shp.prj"),
+    file.path(temp_dir, "ice-aor-county-shp.shx")
+  )
+)
+
+stop()
 
 aor_sf <-
   aor_county_sf |>
