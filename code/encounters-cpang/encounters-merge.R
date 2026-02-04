e7f1f3c8c97aae45404ba81c88c29859839f8c70
Cindy Pang <77863668+cindyjpang@users.noreply.github.com>
2026-02-03 16:05:01 -0800

    encounters & removals

--- code/encounters-cpang/encounters-merge.R
+++ code/encounters-cpang/encounters-merge.R
@@ -5,6 +5,7 @@ rm(list=ls())
 library(dplyr)
 library(tibble)
 library(stringdist)
+library(readr)
 
 # --- Source Functions ---
 source("code/functions/inspect_columns.R")
@@ -12,8 +13,8 @@ source("code/functions/merge_two_df.R")
 
 # --- Read in Combined Data ---
 
-df1 <- read.csv("data/ice-raw/encounters-selected/f082025_combined.csv", stringsAsFactors = FALSE)
-df2 <- read.csv("data/ice-raw/encounters-selected/uwchr_combined.csv", stringsAsFactors = FALSE)
+df1 <- read_csv("data/ice-raw/encounters-selected/f082025_combined.csv")
+df2 <- read_csv("data/ice-raw/encounters-selected/uwchr_combined.csv")
 
 # Step 1 (a). Inspect the columns that are shared and unique between datasets
 venn_1_2b <- inspect_columns(names(df1), names(df2)) 
@@ -24,8 +25,8 @@ df2_cols_new <- c("Responsible_AOR", "Event_Landmark")
 
 merge_1_2 <- merge_dfs(df1, df2, NULL, NULL, df2_cols_old, df2_cols_new)
 
-df12 <- merge_1_2$merged_df
+df12 <- merge_1_2$df_merged
 venn_1_2a <- merge_1_2$venn_after
 
 # --- Write out Merged Data ---
-write.csv(df12, "data/ice-processed/encounters-merged.csv", row.names = FALSE)
+write_feather(df12, "data/ice-processed/encounters-merged.feather")
