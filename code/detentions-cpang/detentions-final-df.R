7dc19bd61ed1efeb850c0ebbcdc5a08bcd0898d1
cindyjpang <cindypang@g.ucla.edu>
2026-03-08 16:47:16 -0700

    detentions final dataframe write-out

--- code/detentions-cpang/detentions-final-df.R
+++ code/detentions-cpang/detentions-final-df.R
@@ -8,3 +8,8 @@ rows_to_drop <- read_feather("code/detentions-cpang/detentions_rows_to_drop.feat
 detentions_clean <- read_feather("data/ice-processed/detentions-merged-with-flags.feather")|>
   mutate(row_id = row_number())|>
   anti_join(rows_to_drop, by = "row_id")
+
+detentions_clean <- detentions_clean |>
+  select(-duplicate_likely, -drop_row, -row_id)
+
+write_feather(detentions_clean, "data/ice-final/detentions-final.feather")
