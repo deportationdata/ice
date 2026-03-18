7129d94785facef3eaaf316a4a67c2a57498bf05
Cindy Pang <77863668+cindyjpang@users.noreply.github.com>
2026-03-17 21:52:03 -0700

    detentions -- which files to merge

--- code/detentions-cpang/detentions-create-df.R
+++ code/detentions-cpang/detentions-create-df.R
@@ -11,63 +11,88 @@ library(stringr)
 library(arrow)
 
 # --- Source Functions ---
-source("code/functions/process_folder_data_v2.R")
+source("code/functions/process_folder_data.R")
 source("code/functions/inspect_columns.R")
-
+source("code/functions/is_not_blank_or_redacted.R")
+source("code/functions/check_dttm_and_convert_to_date.R")
 
 # --- Read all arrests data --- 
 # ROOT: ice/
-df1 <- get_folder_df(
+df1 <- get_folder_df0(
   folder_dir = "data/ice-raw/detentions-selected/2019-ICFO-21307",
   pattern = "\\.xlsx$",
   recursive = TRUE,
   anchor_idx = 2
-)
+)|>
+  mutate(source_file = "2019-ICFO-21307")
 
-df2 <- get_folder_df(
+df1 <- df1 |>
+  select(where(is_not_blank_or_redacted))|>
+  mutate(across(ends_with("_Date"), as.Date))
+
+df2 <- get_folder_df0(
   folder_dir = "data/ice-raw/detentions-selected/2023_ICFO_42034",
   pattern = "\\.xlsx$",
   recursive = TRUE,
   anchor_idx = 2
-)
+)|>
+  mutate(source_file = "2023_ICFO_42034")|>
+  select(where(is_not_blank_or_redacted))|>
+  mutate(across(ends_with("_Date"), as.Date))
 
-df3 <- get_folder_df(
+df3 <- get_folder_df0(
   folder_dir = "data/ice-raw/detentions-selected/2024-ICFO-41855",
   pattern = "\\.xlsx$",
   recursive = TRUE,
   anchor_idx = 2
-)
+)|>
+  mutate(source_file = "2024-ICFO-41855")|>
+  select(where(is_not_blank_or_redacted))|>
+  mutate(across(ends_with("_Date"), as.Date))
 
-df4 <- get_folder_df(
+df3 <- df3 |>
+  mutate(across(where(~ inherits(.x, "POSIXt")), check_dttm_and_convert_to_date))
+
+df4 <- get_folder_df0(
   folder_dir = "data/ice-raw/detentions-selected/120125",
   pattern = "\\.xlsx$",
   recursive = TRUE,
   anchor_idx = 2
-)
+)|>
+  mutate(source_file = "120125")|>
+  select(where(is_not_blank_or_redacted))|>
+  mutate(across(ends_with("_Date"), as.Date))
 
-df5 <- get_folder_df(
+df5 <- get_folder_df0(
   folder_dir = "data/ice-raw/detentions-selected/uwchr",
   pattern = "\\.xlsx$",
   recursive = TRUE,
   anchor_idx = 2
-)
+)|>
+  mutate(source_file = "uwchr")|>
+  select(where(is_not_blank_or_redacted))|>
+  mutate(across(ends_with("_Date"), as.Date))
 
-df6 <- get_folder_df(
+df6 <- get_folder_df0(
   folder_dir = "data/ice-raw/detentions-selected/From-Emily-Excel-X-RIF",
   pattern = "\\.xlsx$",
   recursive = TRUE,
   anchor_idx = 2
-)
+)|>
+  mutate(source_file = "From-Emily-Excel-X-RIF")|>
+  select(where(is_not_blank_or_redacted))|>
+  mutate(across(ends_with("_Date"), as.Date))
 
 library(readxl)
+library(janitor)
 
 # do separate processing for df7 since it's a CSV file
 df7_raw <- read_csv("data/ice-raw/detentions-selected/From-Emily-FOIA-10-2554-527/foia_10_2554_527_NoIDS.csv")
-new_col_names <- colnames(df7_raw)|> str_replace_all("\\.", "_")
-colnames(df7_raw) <- new_col_names
+df7_raw <- df7_raw |> 
+  clean_names(case= "upper_camel")|>
+  rename_with(~ str_replace_all(.x, "(?<=[a-z])(?=[A-Z])", "_"))
 
 # parse date columns in df7
-date_cols_df7 <- colnames(df7_raw)[grepl("(?i)_date", colnames(df7_raw))]
 df7 <- df7_raw |>
   mutate(
     across(
@@ -78,29 +103,19 @@ df7 <- df7_raw |>
                      tz = "UTC")
         )
     )
-  )
+  )|>
+  mutate(source_file = "From-Emily-FOIA-10-2554-527")|>
+  mutate(across(ends_with("_Date"), ~ as.Date(mdy_hm(.x))))
 
-# --- Add source file column ---
-df1 <- df1 %>% 
-  mutate(source_file = "2019-ICFO-21307")
-
-df2 <- df2 %>% 
-  mutate(source_file = "2023_ICFO_42034")
-
-df3 <- df3 %>% 
-  mutate(source_file = "2024-ICFO-41855")
-
-df4 <- df4 %>% 
-  mutate(source_file = "120125")
-
-df5 <- df5 %>% 
-  mutate(source_file = "uwchr")
-
-df6 <- df6 %>% 
-  mutate(source_file = "From-Emily-Excel-X-RIF")
-
-df7 <- df7 %>% 
-  mutate(source_file = "From-Emily-FOIA-10-2554-527")
+df8 <- get_folder_df0(
+  folder_dir = "data/ice-raw/detentions-selected/November 2025 Release",
+  pattern = "\\.xlsx$",
+  recursive = TRUE,
+  anchor_idx = 2
+)|>
+  mutate(source_file = "November 2025 Release")|>
+  select(where(is_not_blank_or_redacted))|>
+  mutate(across(ends_with("_Date"), as.Date))
 
 # --- Write out files ---
 write_feather(df1, "data/ice-raw/detentions-selected/2019-ICFO-21307_combined.feather")
@@ -110,3 +125,4 @@ write_feather(df4, "data/ice-raw/detentions-selected/120125_combined.feather")
 write_feather(df5, "data/ice-raw/detentions-selected/uwchr_combined.feather")
 write_feather(df6, "data/ice-raw/detentions-selected/From-Emily-Excel-X-RIF_combined.feather")
 write_feather(df7, "data/ice-raw/detentions-selected/From-Emily-FOIA-10-2554-527_combined.feather")
+write_feather(df8, "data/ice-raw/detentions-selected/nov2025_combined.feather")
---
+++ code/detentions-cpang/detentions-which-merge.R
@@ -0,0 +1,77 @@
+# --- Clear Memory ---
+rm(list=ls())
+
+# --- Packages ---
+library(dplyr)
+library(tibble)
+library(stringdist)
+library(data.table)
+library(arrow)
+library(ggplot2)
+
+# --- Source Functions ---
+source("code/functions/inspect_columns.R")
+source("code/functions/merge_two_df.R")
+
+df1 <- read_feather("data/ice-raw/detentions-selected/2019-ICFO-21307_combined.feather")|> as_tibble()
+df2 <- read_feather("data/ice-raw/detentions-selected/2023_ICFO_42034_combined.feather")|> as_tibble()
+df3 <- read_feather("data/ice-raw/detentions-selected/2024-ICFO-41855_combined.feather")|> as_tibble()
+df4 <- read_feather("data/ice-raw/detentions-selected/120125_combined.feather")|> as_tibble()
+df5 <- read_feather("data/ice-raw/detentions-selected/uwchr_combined.feather")|> as_tibble()
+df6 <- read_feather("data/ice-raw/detentions-selected/From-Emily-Excel-X-RIF_combined.feather")|> as_tibble()
+df7 <- read_feather("data/ice-raw/detentions-selected/From-Emily-FOIA-10-2554-527_combined.feather")|> as_tibble()
+df8 <- read_feather("data/ice-raw/detentions-selected/nov2025_combined.feather")|> as_tibble()
+
+# determine which files to merge 
+get_weekly_counts <- function(df, date_col_name){
+  df <- df |>
+    rename(Detention_Book_In_Date = date_col_name)
+
+  source_file = unique(df$source_file)
+
+  weekly_counts <- df |>
+  filter(!is.na(Detention_Book_In_Date)) |>
+  mutate(
+    Detention_Book_In_Date = as.Date(Detention_Book_In_Date),
+    week_start = floor_date(Detention_Book_In_Date, unit = "week", week_start = 7)
+  ) |>
+  count(week_start, name = "n") |>
+  arrange(week_start)|>
+  mutate(source_file = source_file)
+  
+  return(weekly_counts)
+}
+
+df1_weekly_counts <- get_weekly_counts(df1, "Book_In_Date")
+df2_weekly_counts <- get_weekly_counts(df2, "Detention_Book_In_Date")
+df3_weekly_counts <- get_weekly_counts(df3, "Book_in_Date_And_Time")
+df4_weekly_counts <- get_weekly_counts(df4, "Book_In_Date_Time")
+df5_weekly_counts <- get_weekly_counts(df5, "Detention_Book_In_Date_And_Time")
+df6_weekly_counts <- get_weekly_counts(df6, "Initial_Intake_Date")
+df7_weekly_counts <- get_weekly_counts(df7, "Book_In_Date")
+df8_weekly_counts <- get_weekly_counts(df8, "Book_In_Date_Time")
+
+all_weekly_counts <- bind_rows(df1_weekly_counts, df2_weekly_counts, df3_weekly_counts, df4_weekly_counts, df5_weekly_counts, df6_weekly_counts, df7_weekly_counts, df8_weekly_counts)|>
+  arrange(week_start)
+
+ggplot(all_weekly_counts,aes(week_start, y=n, color = source_file))+
+  geom_line(alpha = 0.5)+
+  theme_minimal()
+
+boundaries <- all_weekly_counts |> 
+  group_by(source_file) |> 
+  summarise(start = min(week_start), end = max(week_start))
+
+temp <- all_weekly_counts |>
+  filter(source_file == "From-Emily-FOIA-10-2554-527" | source_file == "2024-ICFO-41855"|source_file == "From-Emily-Excel-X-RIF"|source_file == "uwchr"|source_file == "120125")
+
+ggplot(temp ,aes(week_start, y=n, color = source_file))+
+  geom_line(alpha = 0.5)+
+  theme_minimal()
+
+# at minimum, we merge "120125", "2024-ICFO-41855", "From-Emily-Excel-X-RIF", "From-Emily-FOIA-10-2554-527", and "uwchr"  
+rm(df1, df2, df8) # remove these to clear memory space
+gc()
+
+
+
