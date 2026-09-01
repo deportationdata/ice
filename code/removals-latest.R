06c24a3a38f35d398e3acb2c6726b3a5e438bf1c
Phil Neff <philneff@berkeley.edu>
2026-08-31 17:35:11 -0700

    Removals detention stay flagging works, just need to resolve matching criteria and filling in detention flag for duplicates

--- code/removals-latest.R
+++ code/removals-latest.R
@@ -1,6 +1,7 @@
 # ---- Packages ----
 library(tidyverse)
 library(tidylog)
+library(data.table)
 
 # ---- Functions ----
 source("code/functions/check_dttm_and_convert_to_date.R")
@@ -131,7 +132,6 @@ removals_df <-
   select(-msc_code, -msc4, -ucr_violent)
 
 # ---- Construct Duplicate Episode Indicators ----
-library(data.table)
 setDT(removals_df)
 
 removals_df[,
@@ -167,11 +167,11 @@ removals_df[,
            by = .(anonymized_identifier_nona, departed_date)
 ]
 
+# Prefer last record; appear to have better info, but should check
 removals_df <-
   removals_df |>
   as_tibble() |>
   mutate(duplicate_drop_row = duplicate_likely & !episode_last)
-
 
 # ---- Flag for whether removal associated with detention stay ----
 
@@ -179,40 +179,82 @@ pre_join_rows <- nrow(removals_df)
 
 removals_with_anon_id_deduped <- removals_df |>
   filter(!is.na(anonymized_unique_identifier),
-         duplicate_drop_row == FALSE)
+         duplicate_drop_row == FALSE) |>
+  mutate(removal_ID = row_number())
 
 removals_no_anon_id_and_dupes <- removals_df |>
   filter(is.na(anonymized_unique_identifier) | duplicate_drop_row == TRUE)
 
-detentions_df <- detentions_df |>
-  mutate(has_detention = TRUE,
-         stay_book_out_date_time_minus_1 = stay_book_out_date_time - days(1)) |>
-  select(unique_identifier,
-         departure_country,
-         stay_book_out_date_time_minus_1,
-         has_detention) %>% 
+detentions_df_deduped <- detentions_df |>
+  filter(!is.na(unique_identifier)) |>
   distinct(.keep_all=TRUE)
 
-# Create flag for `has_detention` based on matching unique ID, departed date +/- 1 day from stay book out
-# But this creates new rows, how to deal with that?
-removals_with_anon_id_deduped <- removals_with_anon_id_deduped |>
-  mutate(departed_date_plus_1 = departed_date + days(1)) |>
-  left_join(detentions_df, by =
-              join_by(anonymized_unique_identifier == unique_identifier,
-                      departure_country == departure_country,
-                      departed_date_plus_1 >= stay_book_out_date_time_minus_1
-                                           )) |>
-  mutate(id_in_detentions = anonymized_unique_identifier %in% unique(detentions_df$unique_identifier),
-         has_detention = case_when(has_detention == TRUE ~ TRUE,
-                                   TRUE ~ FALSE)) |>
-  select(-departed_date_plus_1, -stay_book_out_date_time_minus_1)
+# ---- Match stays to removals ----
+removal_detention_pairs <-
+  detentions_df_deduped |>
+  select(stay_ID,
+         unique_identifier,
+         stay_book_out_date_time) |>
+  inner_join(
+    removals_with_anon_id_deduped |>
+      select(anonymized_unique_identifier,
+             departed_date,
+             departure_country,
+             removal_ID),
+    by = c("unique_identifier" = "anonymized_unique_identifier"),
+    relationship = "many-to-many"
+  ) |>
+  mutate(
+    time_diff = as.numeric(difftime(
+      stay_book_out_date_time,
+      departed_date,
+      units = "hours"
+    ))
+  ) |>
+  # keep removals 1 days before to 10 days after the stay_book_out_date_time
+  filter(time_diff <= 24 * 1, time_diff >= 24 * -10) |>
+  # then keep the closest removal per book-out and the closest book-out per removal
+  slice_min(
+    order_by = abs(time_diff),
+    n = 1,
+    with_ties = FALSE,
+    by = stay_ID
+  ) |>
+  slice_min(
+    order_by = abs(time_diff),
+    n = 1,
+    with_ties = FALSE,
+    by = removal_ID
+  ) |>
+  select(stay_ID, removal_ID)
+  
+# ---- Merge stays onto removals ----
 
-removals_no_anon_id_and_dupes$has_detention = NA
-removals_no_anon_id_and_dupes$id_in_detentions = NA
+detention_ids <- unique(detentions_df$unique_identifier)
 
-removals_df_out <- rbind(removals_with_anon_id_deduped, removals_no_anon_id_and_dupes)
+removals_with_detentions <- removals_with_anon_id_deduped |>
+  left_join(removal_detention_pairs,
+    by = "removal_ID",
+    relationship = "one-to-one"
+  ) |>
+  mutate(has_detention_stay = !is.na(stay_ID),
+         id_in_detentions =
+           anonymized_unique_identifier %in% detention_ids)
 
-stopifnot(pre_join_rows == nrow(removals_df_out))
+# Better to drop any of these cols that we don't want to output, but keeping for now
+# # Dupes with unique IDs lack detention stay flags
+removals_no_anon_id_and_dupes$removal_ID <- NA
+removals_no_anon_id_and_dupes$stay_ID <- NA
+removals_no_anon_id_and_dupes$has_detention_stay <- NA
+removals_no_anon_id_and_dupes$id_in_detentions <- NA
+
+# # Fill in detention stays for duplicate groups
+# # But this is wrong because only grouped by anon ID, so ignores real multiple unique removals
+# removals_df <- rbind(removals_with_detentions, removals_no_anon_id_and_dupes) |>
+#   group_by(anonymized_unique_identifier) |>
+#   fill(has_detention_stay, stay_ID, removal_ID, id_in_detentions, .direction = "downup")
+
+stopifnot(nrow(removals_df) == pre_join_rows)
 
 # ---- Rename and organize to match other datasets ----
 
@@ -220,16 +262,17 @@ removals_df <- removals_df |>
   rename(
     unique_identifier = anonymized_unique_identifier,
   ) |>
-  relocate(file_original, sheet_original, row_original, .after = last_col())
+  relocate(file_original, sheet_original, row_original, .after = last_col()) |> 
+  arrange(file_original, sheet_original, row_original)
 
 # ---- Save Outputs ----
 
-arrow::write_feather(removals_df, "data/removals-latest.feather")
-removals_df |>
-  mutate(.chunk = ceiling(row_number() / 1e6)) |>
-  group_split(.chunk, .keep = FALSE) |>
-  set_names(~ str_c("Removals (Sheet ", seq_along(.x), ")")) |>
-  writexl::write_xlsx("data/removals-latest.xlsx")
+# arrow::write_feather(removals_df, "data/removals-latest.feather")
+# removals_df |>
+#   mutate(.chunk = ceiling(row_number() / 1e6)) |>
+#   group_split(.chunk, .keep = FALSE) |>
+#   set_names(~ str_c("Removals (Sheet ", seq_along(.x), ")")) |>
+#   writexl::write_xlsx("data/removals-latest.xlsx")
 # haven::write_dta(
 #   removals_df |>
 #     rename(latest_arrest_program_code = latest_arrest_program_current_code), # What is this??
